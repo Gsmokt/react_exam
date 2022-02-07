ff720eebfd717240b7321cf0ef1bf1963ecd8b05
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-02-07 07:45:14 +0100

    new commit

--- src/components/AddProducts/AddProducts.js
+++ src/components/AddProducts/AddProducts.js
@@ -3,46 +3,49 @@ import { useState } from 'react/cjs/react.development';
 import styles from '../../common/styles/Headers.module.scss';
 
 function AddProducts(props) {
-    const [name, setName] = useState('');
-    const [category, setCategory] = useState('');
-    const [product, setProduct] = useState(false);
-    const [invalidName, setInvalidName] = useState(false);
-    const [invalidCategory, setInvalidCategory] = useState(false);
+ const [add, setAdd] = useState({
+    name: '',
+    category: '',
+    product: false,
+    invalidName: false,
+    invalidCategory: false
+ })
 
-    const handleName = (e) => {
-        setName(e.target.value);
-    };
-    const handleCategory = (e) => {
-        setCategory(e.target.value);
-    };
-    const handleProduct = (e) => {
-        setProduct(e.target.checked);
-    };
-    const resetState = () => {
-        setName('');
-        setCategory('');
-        setProduct(false);
-        setInvalidName(false);
-        setInvalidCategory(false);
+    const handleChange = (e) => {
+        setAdd( prevState => {
+            if(e.target.type === 'checkbox') return {...prevState, [e.target.name]: e.target.checked};
+            else return {...prevState, [e.target.name]: e.target.value};
+            });
     };
     const handleClick = () => {
         const products = props.allProducts.map(item => item.nazwa);
-        if(products.includes(name.toLowerCase()) || name === '') {setInvalidName(true); return}
-        else if(category === '') {setInvalidCategory(true); return}
+        if(products.includes(add.name.toLowerCase() || add.name === '')) {setAdd({...add,invalidName:true}); return}
+        else if(add.category === '') {setAdd({...add,invalidCategory: true}); return}
         else {
                 props.addProducts(
-                   {nazwa: name.toLowerCase(),
-                    kategoria: category.toLowerCase(),  
-                    produktSpozywczy: product}
+                   {nazwa: add.name.toLowerCase(),
+                    kategoria: add.category.toLowerCase(),  
+                    produktSpozywczy: add.product}
                     );
-                if(props.addProducts) resetState();
+                if(props.addProducts) setAdd({name: '', category: '', product: false, invalidName: false, invalidCategory: false});
             };
         };
     return (
         <div className={styles.Wrapper}>
-             <label>Nazwa produktu <input autocomplete="off" style={{border: invalidName ? '3px solid red': null}} value={name} type="text" onChange={handleName}/></label>  
-            <label>Katagoria  <input autocomplete="off" style={{border: invalidCategory ? '3px solid red': null}} value={category} onChange={handleCategory} type="text"/></label>   
-               <input value={product} onChange={handleProduct} type="checkbox"/>Produkt spożywczy
+             <label>Nazwa produktu <input name='name' 
+                                          autocomplete="off" 
+                                          style={{border: add.invalidName ? '3px solid red': null}} 
+                                          value={add.name} type="text" 
+                                          onChange={handleChange}/></label>  
+            <label>Katagoria  <input name='category' 
+                                     autocomplete="off" 
+                                     style={{border: add.invalidCategory ? '3px solid red': null}} 
+                                     value={add.category} 
+                                     onChange={handleChange} type="text"/></label>   
+               <input name='product' 
+                      value={add.product} 
+                      onChange={handleChange} 
+                      type="checkbox"/>Produkt spożywczy
             <button onClick={handleClick}>Dodaj nowy produkt</button>
         </div>
       );
